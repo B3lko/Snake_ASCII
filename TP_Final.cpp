d7f34ca32293bc61b0157e1efcac9fbf0b05a38a
Federico Eigler <fedeeigler@hotmail.com>
2022-08-29 17:45:30 -0300

    Class Game
    Creacion de la clase Game. Primer commit

---
+++ TP_Final.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <conio2.h>
+using namespace std;
+
+class Game{
+private:
+public:
+	Game(){
+		
+	}
+	void Update(){
+		
+	}
+	void Run(){
+		Update();
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+	Game game;
+	game.Run();
+	return 0;
+}
+
